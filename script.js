de90a884bd0566684e17bc92eb91b2dbaa69e504
ferb00000100 <fhlh@protonmail.com>
2019-10-02 14:38:30 -0600

    index.html changes added timer timer class to html changed input elements to buttons script.js changes added new questions in object refactored all the variables added setTimer() function add if/else statements for the correct answers added localstorage for initials and scores

--- script.js
+++ script.js
@@ -1,46 +1,110 @@
 
 var questions = [{
-		title: "Commonly used data types DO NOT include:",
-		choices: [
-			"strings",
-			"booleans",
-			"alerts",
-			"numbers"
-		],
-		answer: "alerts"
+		title: "What is the correct way to access the 'models' of an object named 'cars' ",
+		choices: ["obj.cars[models]", "cars.models", "objCars.models", "models.cars"],
+		answer: "cars.models"
 	},{
-		title: "The condition in an if / else statement is enclosed within ____.",
-		choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
-		answer: "parentheses"
-
-}];
+		title: "What is the correct way to convert an array into a string",
+		choices: ["x.splice(4, 1,'');", "x.toString();", "x.split(');", "x.array();"],
+		answer: "x.toString();"
+	},{
+		title: "How can you view logs when running JavaScript?",
+		choices: ["server system logs", "http logs", "browser console logs", "javascript application logs"],
+		answer: "browser console logs"
+	},{
+		title: "How would you declare the variable 'x' in Javascript?",
+		choices: ["var x", "x", "int x", "x = x"],
+		answer: "var x"
+	},{
+		title: "What is one way to get user input into JavaScript?",
+		choices: ["getInput()", "raw_input()", "$x = <STDIN", "prompt()"],
+		answer: "prompt()"
+}  ];
 
 var startButton = document.querySelector("#startQuiz");
-var submitQ1 = document.querySelector("#sumbitQ1");
-
+var choice1 = document.querySelector("#choice1");
+var choice2 = document.querySelector("#choice2");
+var choice3 = document.querySelector("#choice3");
+var choice4 = document.querySelector("#choice4");
 var question = document.querySelector("#question");
-var answerStrings = document.querySelector("#question1");
-var answeBooleans = document.querySelector("#question1");
-var answerAlerts = document.querySelector("#question1");
-var answerNumbers = document.querySelector("#question1");
+var questionWindow = document.querySelector("#qWindow");
+var timer = document.querySelector("#timer");
+var secondsLeft = 76;
+
+function setTime() {
+	var timerInterval = setInterval(function() {
+		secondsLeft--;
+		timer.textContent = "timer " + secondsLeft;
+
+		if(secondsLeft === 0) {
+			clearInterval(timerInterval);
+			console.log("COMPLETED");
+			// sendMessage();
+		}
+
+	}, 1000);
+}
 
 
-// function questions(x)
-function begin(){
-	x=1;
-	document.getElementById("begin").style.display = "none";
-
-	document.getElementById("q1").style.display = "block";
-	// questions.textContent = questions.title;
-
-	question.textContent = questions[0].title;
-	// questions(5);
+function displayQuestions(i) {
+	question.textContent = questions[i].title;
+	choice1.textContent = questions[i].choices[0];
+	choice2.textContent = questions[i].choices[1];
+	choice3.textContent = questions[i].choices[2];
+	choice4.textContent = questions[i].choices[3];
 
 }
 
-document.getElementById("q1").style.display = "none";
-// startButton.addEventListener("click", startTimer);
+function storeScore(initials, score) {
+	console.log(initials);
+	console.log(score);
+	localStorage.setItem(JSON.stringify(initials),JSON.stringify(score));
+
+}
+
+function startQuestions(){
+	var initials = prompt("type in your initials");
+	var score = 0;
+	var i = 0;
+
+	document.getElementById("qWindow").style.display = "block";
+	displayQuestions(i);
+	questionWindow.addEventListener("click", function (event) {
+		console.log(event);
+		if (event.target.textContent === questions[i].answer) {
+			document.getElementById("qWindow").style.display = "none";
+			console.log("Correct Answer");
+			score++;
+			i++;
+			if (i <=4) {
+				displayQuestions(i);
+				document.getElementById("qWindow").style.display = "block";
+			}
+			else {
+				secondsLeft = 1;
+				storeScore(initials, score);
+			}
+		}
+		else {
+			secondsLeft = secondsLeft - 15;
+			i++;
+			if (i <=4) {
+				displayQuestions(i);
+				document.getElementById("qWindow").style.display = "block";
+			}
+		}
+
+	})
+
+}
+
+function begin(){
+
+	document.getElementById("begin").style.display = "none";
+	setTime();
+	startQuestions();
+
+}
+
+document.getElementById("qWindow").style.display = "none";
 startButton.addEventListener("click", begin);
-
-
-// test  = questions.choices;
